8afe7cf1fc1b25038477485b7e7b17884997ca92
NIkhil Sonawane <51852902+codenikhil1@users.noreply.github.com>
2019-12-19 22:11:31 +0530

    Create player.java
    player class--use to create player and define each player and its various functions.

---
+++ player.java
@@ -0,0 +1,35 @@
+package TicTac;
+
+public class player {
+	private String name;
+	private char symbol;
+	
+	public player(String name,char symbol) {
+		setname(name);
+		setsymbol(symbol);
+	}
+	
+	public void setname(String name){
+		if(!name.isEmpty()) {
+			this.name=name;
+			
+		}
+	}
+	public void setsymbol(char symbol){
+		if(symbol!='\0') {
+			this.symbol=symbol;
+			
+		}
+	}
+	public String getname(){
+		return name;
+	}
+	public char getsymbol() {
+		return symbol;
+	}
+	
+	
+	
+	
+	
+}
